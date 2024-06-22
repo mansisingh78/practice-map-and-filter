f55c296ab25a0d2b40ca457beb0a123d090e42c9
Mansi singh <mansi123singh1234@gmail.com>
2024-06-22 22:10:05 +0530

    Question 9 Solved

--- script.js
+++ script.js
@@ -74,4 +74,16 @@ const peoples = [
 ];
 
 const filterPeoples = peoples.filter((people) => people.age > 30);
-console.log(filterPeoples);
+// console.log(filterPeoples);
+
+// 9. Write an ES6 function that takes an array of objects with the properties name and score, and
+// returns an array with only the objects that have a score greater than 80.
+
+const students = [
+  { name: "Aditya", score: 85 },
+  { name: "Bob", score: 75 },
+  { name: "Charlie", score: 90 },
+];
+
+const student = students.filter((student) => student.score > 80);
+console.log(student);
